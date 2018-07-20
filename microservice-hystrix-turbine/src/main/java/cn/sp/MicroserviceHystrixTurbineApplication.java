c9da8b10850376a60ed74918b546c07dc51f37a0
ship <shipeng@mmtvip.com>
2018-07-20 14:09:37 +0800

    使用turbine聚合监控数据

---
+++ microservice-hystrix-turbine/src/main/java/cn/sp/MicroserviceHystrixTurbineApplication.java
@@ -0,0 +1,14 @@
+package cn.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@EnableTurbine
+@SpringBootApplication
+public class MicroserviceHystrixTurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceHystrixTurbineApplication.class, args);
+	}
+}
