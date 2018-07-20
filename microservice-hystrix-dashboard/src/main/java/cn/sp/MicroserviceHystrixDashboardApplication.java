b82705a760d97d6653f3bc32747c3e1c16805984
ship <shipeng@mmtvip.com>
2018-07-20 14:08:41 +0800

    使用dashboard数据可视化

---
+++ microservice-hystrix-dashboard/src/main/java/cn/sp/MicroserviceHystrixDashboardApplication.java
@@ -0,0 +1,14 @@
+package cn.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableHystrixDashboard
+@SpringBootApplication
+public class MicroserviceHystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceHystrixDashboardApplication.class, args);
+	}
+}
